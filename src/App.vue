bb8bbf34629b7cf930a05c97debf6b97570abcbd
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-12-18 14:44:58 +0000

    Adds CountriesList and ListItem

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div>
+    <h1>Countries</h1>
+    <div class="main-container">
+
+    </div>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
+  data(){
+    return {
+      countries: [],
+      selectedCountry: null
+    };
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+  },
   components: {
-    HelloWorld
+    // "countries-list": CountriesList
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
---
+++ src/components/CountriesList.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+
+</template>
+
+<script>
+import ListItem from './ListItem.vue'
+
+export default {
+  name: 'countries-list',
+  props: ['countries'],
+  components: {
+    "list-item": ListItem
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
